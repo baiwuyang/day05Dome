59d588a7d6c9e49488c0febbc409b3cbd1f30362
baiwuyang <x827701008@qq.com>
2018-03-07 16:15:05 +0800

    嘻嘻嘻

--- app/src/main/java/com/example/day05dome/MainActivity.java
+++ app/src/main/java/com/example/day05dome/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+        ////哈哈哈哈或或或或或或
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
